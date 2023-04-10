14b4a40e626c64ea4867fbfb95a0ff307c2c7250
ogabek542 <otaxonovo22222@gmail.com>
2023-04-10 23:07:59 +0500

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+// const menu = document.querySelector('.navmenu')
+// const menuBtn = document.querySelector('.lefticon')
+// const menuClose = document.querySelector('.navmenuclose')
+// const tools = document.querySelector('.navbootom')
+// const extraElements = document.querySelector('.navicons')
+
+// menuBtn.addEventListener('click', () => {
+//   menu.classList.add('.show')
+// })
+
+// menuClose.addEventListener('click', () => {
+//   menu.classList.remove('show')
+// })
+
+// const moveElementsForMenu = () => {
+//   if (window.innerWidth < 1024 && menu) {
+//     menu.insertAdjacentElement('beforeend', extraElements);
+//   } else {
+//     tools.insertAdjacentElement('afterbegin', extraElements);
+//   }
+// }
+
+// window.addEventListener('load', moveElementsForMenu)
+// window.addEventListener('resize', moveElementsForMenu)
